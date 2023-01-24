02ec87a4eec6805e09dab13f1273a089bf088c99
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-24 12:48:53 +0300

    set up routing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
 
 const app = express();
 
@@ -12,9 +14,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('api/v1/auth', authRouter);
+app.use('api/v1/jobs', jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
