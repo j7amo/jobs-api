b172980380e35b2176c85966820dbeb6ddcafc82
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-25 14:36:17 +0300

    add Swagger docs

--- src/app.js
+++ src/app.js
@@ -8,6 +8,12 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const connectDB = require('./db/connect');
 const jobsRouter = require('./routes/jobs');
 const authRouter = require('./routes/auth');
@@ -48,7 +54,8 @@ app.use(xss());
 
 // just to be able to quickly check if application is successfully deployed
 // we set up a simple route
-app.get('/', (req, res) => res.send('Welcome to Jobs API'));
+app.get('/', (req, res) => res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>'));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
